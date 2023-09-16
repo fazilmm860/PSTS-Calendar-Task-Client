08b095cccc182e118bbf94c949cb64ef0cdba219
Mohamed Fazil M <fazilmm860@gmail.com>
2023-09-17 00:36:55 +0530

    Dashboard created

---
+++ src/components/LoginPage/Dashboard.js
@@ -0,0 +1,72 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { LoginContext } from './ContextProvider/Context';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import InputShortener from './InputShortner';
+
+
+import BackgroundAnimate from './BackgroundAnimate';
+
+const Dashboard = () => {
+
+    const { logindata, setLoginData } = useContext(LoginContext);
+
+    const [data, setData] = useState(false);
+
+    const [inputValue, setInputValue] = useState("");
+    const history = useNavigate();
+
+    const DashboardValid = async () => {
+        let token = localStorage.getItem("usersdatatoken");
+
+        const res = await fetch("https://url-shorten-forgot-pass.onrender.com/validuser", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        });
+
+        const data = await res.json();
+
+        if (data.status == 401 || !data) {
+            history("*");
+        } else {
+            console.log("user verify");
+            setLoginData(data)
+            history("/dash");
+        }
+    }
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            DashboardValid();
+            setData(true)
+        }, 2000)
+
+    }, [])
+
+    return (
+
+        <>
+            <BackgroundAnimate />
+            {
+                data ? <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <img src="./man.png" style={{ width: "200px", marginTop: 20 }} alt="" />
+                    <h1>User Email:{logindata ? logindata.ValidUserOne.email : ""}</h1>
+                </div> : <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh" }}>
+                    Loading... &nbsp;
+                    <CircularProgress />
+                </Box>
+            }
+            <InputShortener setInputValue={setInputValue} inputValue={inputValue} />
+
+
+        </>
+
+    )
+}
+
+export default Dashboard
