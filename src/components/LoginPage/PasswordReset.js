9841e9e5a8c40c723ae6454b1991cc2ddce8bfb1
Mohamed Fazil M <fazilmm860@gmail.com>
2023-09-17 00:53:04 +0530

    PasswordReset page create

--- src/components/LoginPage/PasswordReset.js
+++ src/components/LoginPage/PasswordReset.js
@@ -23,7 +23,7 @@ const PasswordReset = () => {
                 position: "top-center"
             });
         } else {
-            const res = await fetch("/sendpasswordlink", {
+            const res = await fetch("http://localhost:4001/api/sendpasswordlink", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
